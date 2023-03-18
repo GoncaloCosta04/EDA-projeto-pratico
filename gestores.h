3e17534f55372f407572b2711777fc9cab2bd732
Gonçalo <a26024@alunos.ipca.pt>
2023-03-18 14:24:11 +0000

    assinaturas das funções gestores

--- gestores.h
+++ gestores.h
@@ -8,3 +8,14 @@ int contacto;
 char username[100];
 struct gestor* seguinte;
 } Gestor;
+
+
+int guardarGestor(Gestor* inicio);
+Gestor* lerGestor();
+int existeGestor(Gestor* inicio, int cod);
+Gestor* inserirGestor(Gestor* inicio, int cod_g,int password,int contacto, char username[]);
+Gestor* removerGestor(Gestor* inicio, int cod);
+int  alterargestor(Gestor *inicio, int cod, int pass, char novo_username[100], int nova_password);
+void listarGestor(Gestor * inicio);
+int guardarGestor(Gestor* inicio);
+int guardarGestorembinario(Gestor* inicio);
