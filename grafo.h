0b99bf6f6592d9d7e520ee402784a47123f3692a
GoncaloCosta04 <128237370+GoncaloCosta04@users.noreply.github.com>
2023-05-27 23:26:32 +0100

    ficheiro .h para a fase 2

---
+++ grafo.h
@@ -0,0 +1,66 @@
+#include "meio.h"
+#include "cliente.h"
+#include "gestor.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct registo1 
+{
+    char vertice[250]; 
+    struct registo2 *adjacentes;
+    Meio* meios;
+    Cliente* clientes;
+    int visitado;
+    float distancia;
+    struct registo1 *seguinte;
+} *Grafo;
+
+typedef struct registo2 {
+    char vertice[250];
+    float peso;
+    Meio* meios;
+    Grafo grafos;  
+    int visitado;
+    struct registo2* seguinte;
+} *Adjacente;
+
+
+int criarVertice(Grafo *g, char novoId[]);
+int existeVertice(Grafo g, char vertice[]); 
+void saveVertices(Grafo g);
+void saveVerticesbin(Grafo g); 
+void readVertices(Grafo* g);
+int criarAresta(Grafo g, char vOrigem[], char vDestino[], float peso);
+void saveArestas(Grafo g);
+void saveArestasbin(Grafo g); 
+void readArestas(Grafo* g);
+int inserirMeioNoGrafo(Grafo g, char geocodigo[], int codigom, char tipo[], float bateriam, float autonomia, float preco, int estado, float espacom);
+void saveMeios(Grafo g);
+void saveMeiosbin(Grafo g);
+void readMeios(Grafo* g);
+int inserirClienteNoGrafo(Grafo g, char geocodigo[], int codigoc, char nome[], int NIF, float saldo, char morada[]);
+void saveClientes(Grafo g);
+void saveClientesbin(Grafo g);
+void readClientes(Grafo* g);
+Grafo procurarVertice(Grafo g, const char* geocodigo); 
+void listarMeiosGrafo(Grafo g, char vertice[]);
+void listarClientesGrafo(Grafo g, char vertice[]);
+void listarMeiosBaixaBateria(Grafo g);
+
+
+
+
+int clienteExiste(Grafo g, char geoccode[]);
+Grafo findNode(Grafo g, char geocode[]);
+void traverseEdgesDFS(Grafo node, char type[], float radius, float currentWeight, Grafo g);
+void listVehiclesPerRadius(Grafo g, char geocode[], char type[], float radius);
+void Visitados(Grafo g);    
+void printPathAndWeight(Grafo node, float weight);
+void IItraverseEdgesDFS(Grafo node, float batteryThreshold, float currentWeight, Grafo g, int searchType, char path[], float pathWeight);
+void findPathsLowBattery(Grafo g, float batteryThreshold);
+void listAllPathsLowBattery(Grafo g, float batteryThreshold);
+void resetVisitedFlag(Grafo g);
+
+
